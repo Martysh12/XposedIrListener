dfa9149f417abbd1350751c42fb7f75f5156e048
Martin K <martyshkon@gmail.com>
2023-11-22 21:59:42 +0200

    Re-write IrListener in Kotlin

--- app/src/main/java/com/martysh12/xposedirlistener/IrListener.kt
+++ app/src/main/java/com/martysh12/xposedirlistener/IrListener.kt
@@ -1,38 +1,48 @@
-package com.martysh12.xposedirlistener;
+package com.martysh12.xposedirlistener
 
-import android.hardware.ConsumerIrManager;
-import android.os.Environment;
+import android.hardware.ConsumerIrManager
+import android.os.Environment
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
+import java.io.FileWriter
+import java.io.IOException
+import java.util.Arrays
+import java.util.stream.Collectors
+import kotlin.Int
+import kotlin.IntArray
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+class IrListener : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: LoadPackageParam) {
+        XposedBridge.log("Hooking transmit(int, int[])...")
 
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+        XposedHelpers.findAndHookMethod(
+            ConsumerIrManager::class.java,
+            "transmit",
+            Int::class.javaPrimitiveType,
+            IntArray::class.java,
+            object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val frequency = param.args[0] as Int
+                    val pattern = param.args[1] as IntArray
 
-public class IrListener implements IXposedHookLoadPackage {
-    @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        XposedBridge.log("Hooking transmit(int, int[])...");
+                    val patternString =
+                        Arrays.stream(pattern).mapToObj { i: Int -> i.toString() }
+                            .collect(Collectors.joining(","))
 
-        XposedHelpers.findAndHookMethod(ConsumerIrManager.class, "transmit", int.class, int[].class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) {
-                int frequency = (int) param.args[0];
-                int[] pattern = (int[]) param.args[1];
-
-                String line = lpparam.packageName + " " + frequency + " " + Arrays.stream(pattern).mapToObj(String::valueOf).collect(Collectors.joining(",")) + "\n";
-
-                try (FileWriter log = new FileWriter(Environment.getExternalStorageDirectory().getPath() + "/Documents/ir.txt", true)) {
-                    log.append(line);
-                } catch (IOException e) {
-                    XposedBridge.log(e);
+                    val line = "${lpparam.packageName} $frequency $patternString\n"
+                    
+                    try {
+                        FileWriter(
+                            Environment.getExternalStorageDirectory().path + "/Documents/ir.txt",
+                            true
+                        ).use { log -> log.append(line) }
+                    } catch (e: IOException) {
+                        XposedBridge.log(e)
+                    }
                 }
-            }
-        });
+            })
     }
 }
